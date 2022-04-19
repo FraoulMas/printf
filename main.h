7930d4bf4ff647059dd0288151cde0b0cbed5e33
fraoulmas <fraoulmas@gmail.com>
2022-04-19 22:52:31 +0300

    fixed

---
+++ main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct format - a type defined structure
+ * @opt: pointer to char type
+ * @call_fun: pointer to call the function
+ */
+
+
+typedef struct format
+{
+	char *opt;
+	int (*call_fun)();
+} format_t;
+
+
+
+
+
+int _printf(const char *format, ...);
+int parse_format(const char *format, format_t get_opt[], va_list valist);
+int set_string(va_list valist);
+int set_char(va_list valist);
+int _putchar(char c);
+int set_decimal(va_list valist);
+int print_percent(__attribute__((unused))va_list valist);
+int length_of_integer(int n);
+int get_length(char *str __attribute__((__unused__)), va_list valist);
+
+#endif
